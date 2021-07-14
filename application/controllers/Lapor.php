0b75631fac1d948f1a9505c225910d89d7025ebe
syahacbar <syahacbar@gmail.com>
2021-07-14 21:27:27 +0900

    20210714-2127
    Update form process

--- application/controllers/Lapor.php
+++ application/controllers/Lapor.php
@@ -31,7 +31,7 @@ class Lapor extends CI_Controller{
         $get_kab = $this->db->select('*')->from('regencies')->get();
         $data['laporan'] = $this->Laporan_model->get_all_laporan();
         $data['kabupaten'] = $get_kab->result();
-        
+           
         $data['_view'] = 'home';
         $this->load->view('home',$data);
     }
@@ -65,6 +65,44 @@ class Lapor extends CI_Controller{
         }
         echo $data;
     }
+
+    function add()
+    {   
+        $this->load->library('form_validation');
+        $this->form_validation->set_rules('nik','NIK','required');
+        
+        if($this->form_validation->run())     
+        {   
+            $params = array(
+                'tgl_laporan' => $this->input->post('tgl_laporan'),
+                'nama_pelapor' => $this->input->post('nama_pelapor'),
+                'nik' => $this->input->post('nik'),
+                'alamat_pelapor' => $this->input->post('alamat_pelapor'),
+                'kab_pelapor' => $this->input->post('kab_pelapor'),
+                'kec_pelapor' => $this->input->post('kec_pelapor'),
+                'des_pelapor' => $this->input->post('des_pelapor'),
+                'no_hp' => $this->input->post('no_hp'),
+                'email' => $this->input->post('email'),
+                'foto_ktp' => $this->input->post('foto_ktp'),
+                'pengaduan' => $this->input->post('pengaduan'),
+                'infrastruktur' => $this->input->post('infrastruktur'),
+                'lokasi_namajalan' => $this->input->post('lokasi_namajalan'),
+                'lokasi_kabkota' => $this->input->post('lokasi_kabkota'),
+                'lokasi_distrik' => $this->input->post('lokasi_distrik'),
+                'latitude' => $this->input->post('latitude'),
+                'longitude' => $this->input->post('longitude'),
+                'dokumentasi' => $this->input->post('dokumentasi'),
+            );
+            
+            $laporan_id = $this->Laporan_model->add_laporan($params);
+            redirect('laporan/index');
+        }
+        else
+        {            
+            redirect('lapor');
+        }
+    }  
+
 
 
 
