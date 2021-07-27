071af68205b020379b3fea8dd7c31b581c4dc3cf
syahacbar <syahacbar@gmail.com>
2021-07-28 00:57:58 +0900

    admin panel

---
+++ application/controllers/Admin.php
@@ -0,0 +1,29 @@
+<?php
+ 
+class Admin extends CI_Controller{
+    function __construct()
+    {
+        parent::__construct();
+        
+    }
+
+    function index()
+    {
+        
+        $get_kab = $this->db->query("SELECT * FROM wilayah_2020 WHERE LENGTH(kode) = 5 AND kode LIKE '92%' ORDER BY kode ASC");
+        $data['kabupaten'] = $get_kab->result();
+
+        $data['_view'] = 'admin/dashboard';
+        $this->load->view('admin/layout',$data);
+    }
+
+    function jalan()
+    {
+        $get_kab = $this->db->query("SELECT * FROM wilayah_2020 WHERE LENGTH(kode) = 5 AND kode LIKE '92%' ORDER BY kode ASC");
+        $data['kabupaten'] = $get_kab->result();
+        
+        $data['_view'] = 'admin/jalan';
+        $this->load->view('admin/layout',$data);
+    }
+    
+}
