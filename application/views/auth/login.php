d5cfdd07b2a2b451f98c4a4bcff53092fa733638
syahacbar <syahacbar@gmail.com>
2021-08-06 03:58:51 +0900

    Update login.php

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -36,7 +36,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="description" content="" />
         <meta name="author" content="" />
-        <title>Login - SB Admin</title>
+        <title>Login - Panel Admin Sisikat</title>
         <link href="<?php echo base_url();?>resources/admintheme/css/styles.css" rel="stylesheet" />
         <link href="<?php echo base_url();?>resources/admintheme/css/login-css.css" rel="stylesheet" />
         <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js" crossorigin="anonymous"></script>
