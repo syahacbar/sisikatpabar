c6c217abc4c718258b996b54d15745136e6bc7df
ariesrutung <ariesrutung@gmail.com>
2021-08-08 04:14:12 +0900

    Add page SK Ruas Jalan dan tambah table databases

---
+++ application/models/M_skruasjalan.php
@@ -0,0 +1,11 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class M_skruasjalan extends CI_Model {
+
+    public function get_all_skruasjalan()
+	{
+		$query = $this->db->get('skruasjalan');  
+		return $query->result_array();
+	}
+}
