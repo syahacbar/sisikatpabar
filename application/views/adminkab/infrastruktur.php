fc23597442875be5ed2bbecbe3b401b1b1cffb90
ariesrutung <ariesrutung@gmail.com>
2021-08-10 09:50:56 +0900

    data modal detail

--- application/views/adminkab/infrastruktur.php
+++ application/views/adminkab/infrastruktur.php
@@ -14,12 +14,12 @@
                         <th>Lokasi</th>
                         <th>Kec./Distrik</th>
                         <th>Kab./Kota</th>
-                        <th>Status</th>
-                        <th>Detail</th>
+                        <th>Aksi</th>
                     </tr>
                 </thead>
                 <tbody>
                 <?php $no=1; foreach($laporan as $lap) { ?>
+                    
                     <tr>
                         <td><?php echo $no++;?></td>
                         <td><?php echo $lap['tgl_laporan'];?></td>
@@ -28,13 +28,27 @@
                         <td><?php echo $lap['lokasidistrik'];?></td>
                         <td><?php echo $lap['lokasikabkota'];?></td>
                         <td>
-                            <a href="" class="btn btn-warning">Menunggu</a>
-                            <a href="" class="btn btn-success">Diterima</a>
-                            <a href="" class="btn btn-danger">Ditolak</a>
-                        </td>
+                            <a
+                                id="#modalDetail"
+                                data-nik="<?php echo $lap['nik'] ?>"
+                                data-ktp="<?php echo $lap['ktp'] ?>"
+                                data-nama="<?php echo $lap['nama_pelapor'] ?>"
+                                data-alamat="<?php echo $lap['alamat_pelapor']." ".$lap['des_pelapor']." ".$lap['kec_pelapor']." ".$lap['kab_pelapor'] ?>"
+                                data-email="<?php echo $lap['email'] ?>"
+                                data-nohp="<?php echo $lap['no_hp'] ?>"
 
-                        <td>
-                            <a id="ISBN-001122" data-nik="<?php echo $lap['nik']; ?>" data-bs-target="#ISBN-001122" data-bs-toggle="modal" class="btn btn-info" >
+                                data-infra="<?php echo $lap['infrastruktur'] ?>"
+                                data-latitude="<?php echo "Latitude:" ." " .$lap['latitude'] ?>"
+                                data-longitude="<?php echo "Longitude:"." ".$lap['longitude'] ?>"
+                                data-ruasjalan="<?php echo $lap['lokasi_namajalan'] ?>"
+                                data-lokasi="<?php echo $lap['lokasi_kabkota']." ".$lap['lokasi_distrik'] ?>"
+                                data-pengaduan="<?php echo $lap['pengaduan'] ?>"
+                                data-dokumentasi1="<?php echo $lap['dokumentasi1'] ?>"
+                                data-dokumentasi2="<?php echo $lap['dokumentasi2'] ?>"
+                                data-dokumentasi3="<?php echo $lap['dokumentasi3'] ?>"
+
+
+                                data-bs-target="#modalDetail" data-bs-toggle="modal" class="modalDetail btn btn-info" >
                                 <i class="fas fa-external-link-alt"></i>
                             </a>
                         </td>
@@ -48,16 +62,62 @@
 
 <script>
     $(document).ready(function(){
-        $("#ISBN-001122").click(function(){
-            $("#ISBN-001122").modal('show');
+        $("#modalDetail").click(function(){
+            $("#modalDetail").modal('show');
         });
-    });
-</script>
-
-<script>
-    $(function() {
         $('#closeBtn').click(function() {
-            $('#exampleModalLong').modal('hide');
+            $('#modalDetail').modal('hide');
         });
+        $(document).on("click", ".modalDetail", function () {
+            // identitas pelapor
+             var nik = $(this).data('nik');
+             $(".modal-body #nikModal").text(nik);
+
+             var ktp = $(this).data('ktp');
+             const img = document.getElementById("imgModal");
+            img.src = "<?php echo base_url('upload/ktp/');?>"+ktp;
+
+            var nama = $(this).data('nama');
+            $(".modal-body #namaModal").text( nama );
+
+            var alamat = $(this).data('alamat');
+            $(".modal-body #alamatModal").text( alamat );
+
+            var email = $(this).data('email');
+            $(".modal-body #emailModal").text( email );
+
+            var hp = $(this).data('nohp');
+            $(".modal-body #nohpModal").text( hp );
+
+            // data laporan
+            var infra = $(this).data('infra');
+            $(".modal-body #infra").text(infra);
+
+            var latitude = $(this).data('latitude');
+            var longitude = $(this).data('longitude');
+            $(".modal-body #latitude").text(latitude );
+            $(".modal-body #longitude").text(longitude );
+            
+
+            var ruasjalan = $(this).data('ruasjalan');
+            $(".modal-body #ruasjalan").text(ruasjalan);
+
+            var lokasi = $(this).data('lokasi');
+            $(".modal-body #lokasi").text(lokasi);
+
+            var pengaduan= $(this).data('pengaduan');
+            $(".modal-body #pengaduan").text(pengaduan);
+
+            var dokumentasi1 = $(this).data('dokumentasi1');
+            var dokumentasi2 = $(this).data('dokumentasi2');
+            var dokumentasi3 = $(this).data('dokumentasi3');
+             const img1 = document.getElementById("dok1");
+             const img2 = document.getElementById("dok2");
+             const img3 = document.getElementById("dok3");
+            img1.src = "<?php echo base_url('upload/dokumentasi/');?>"+dokumentasi1;
+            img2.src = "<?php echo base_url('upload/dokumentasi/');?>"+dokumentasi2;
+            img3.src = "<?php echo base_url('upload/dokumentasi/');?>"+dokumentasi3;
+        });
+
     });
 </script>
--- application/views/adminkab/layout.php
+++ application/views/adminkab/layout.php
@@ -112,110 +112,109 @@
             </div>
         </div>
 
-
-        <!-- Modal Detail Setiap Infrastruktur -->
-        <div class="modal fade" id="ISBN-001122" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLongTitle">Laporan</h5>
-                <button id="closeBtn" type="button" class="close" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                <h5 class="card-title">Data Pelapor</h5>
-            <div class="row">
-                <div id="idcard" class="col-sm-5">
-                    <div class="card">
-                    <div class="card-body">
-                        <img src="http://1.bp.blogspot.com/-cKbR2Cw8BLU/VrLaPvhz9pI/AAAAAAAAAcE/Pe9LhaTN1sY/s1600/Scan%2BKTP.JPG" alt="id-card" >
-                    </div>
-                    </div>
+    <!-- Modal Detail Setiap Infrastruktur -->
+    <div class="modal fade" id="modalDetail" tabindex="-1" role="dialog" aria-labelledby="modalDetailTitle" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="modalDetailTitle">Laporan</h5>
+                        <button id="closeBtn" type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                    </button>
                 </div>
-                
-                <div id="identity" class="col-sm-7">
-                    <div class="card">
-                    <div class="card-body">
-                        <table class="table">
-                        <tbody>
-                            <tr>
-                            <td width="30%">NIK</td>
-                            <td>:</td>
-                            <td>1904502455xxx</td>
-                            </tr>
-                            <tr>
-                            <td width="30%">Nama Lengkap</td>
-                            <td>:</td>
-                            <td>Gajah Mada Bin Gajah Duduk</td>
-                            </tr>
-                            <tr>
-                            <td width="30%">Alamat Lengkap</td>
-                            <td>:</td>
-                            <td>(Alamat Rumah, RT/RW, Kab./Kota, Kec./Distrik, dan Kel./Desa)</td>
-                            </tr>
-                            <tr>
-                            <td width="30%">Email</td>
-                            <td>:</td>
-                            <td>myemail@email.com</td>
-                            </tr>
-                            <tr>
-                            <td width="30%">Nomor HP</td>
-                            <td>:</td>
-                            <td>0812474147444</td>
-                            </tr>
-                        </tbody>
-                        </table>
+                <div class="modal-body">
+                    <h5 class="card-title">Data Pelapor</h5>
+                <div class="row">
+                    <div id="idcard" class="col-sm-5">
+                        <div class="card">
+                        <div class="card-body">
+                            <img src="xxx" id="imgModal" alt="id-card" >
+                        </div>
+                        </div>
                     </div>
+                    
+                    <div id="identity" class="col-sm-7">
+                        <div class="card">
+                        <div class="card-body">
+                            <table class="table">
+                            <tbody>
+                                <tr>
+                                <td width="30%">NIK</td>
+                                <td>:</td>
+                                <td><span id="nikModal"></span></td>
+                                </tr>
+                                <tr>
+                                <td width="30%">Nama Lengkap</td>
+                                <td>:</td>
+                                <td><span id="namaModal"></span></td>
+                                </tr>
+                                <tr>
+                                <td width="30%">Alamat Lengkap</td>
+                                <td>:</td>
+                                <td><span id="alamatModal"></span></td>
+                                </tr>
+                                <tr>
+                                <td width="30%">Email</td>
+                                <td>:</td>
+                                <td><span id="emailModal"></span></td>
+                                </tr>
+                                <tr>
+                                <td width="30%">Nomor HP</td>
+                                <td>:</td>
+                                <td><span id="nohpModal"></span></td>
+                                </tr>
+                            </tbody>
+                            </table>
+                        </div>
+                        </div>
                     </div>
-                </div>
-                <div id="report" class="col-sm-12">
-                    <h4 class="card-title">Detail Laporan</h4>
-                    <div class="card">
-                    <div class="card-body">
-                        <table class="table">
-                        <tbody>
-                            <tr>
-                            <td width="35%">Jenis Infrastruktur</td>
-                            <td>:</td>
-                            <td>Jalan</td>
-                            </tr>
-                            <tr>
-                            <td width="35%">Koordinat Lokasi</td>
-                            <td>:</td>
-                            <td>Latitude: | Longitude: </td>
-                            </tr>
-                            <tr>
-                            <td width="35%">Nama Ruas Jalan</td>
-                            <td>:</td>
-                            <td>Jalan Pertanian Kebuk Jeruk Kebun Anggur</td>
-                            </tr>
-                            <tr>
-                            <td width="35%">Alamat Lengkap</td>
-                            <td>:</td>
-                            <td>(Kab./Kota, Kec./Distrik, dan Kel./Desa)</td>
-                            </tr>
-                        </tbody>
-                        </table>
-                        <h5>Isi Laporan:</h5>
-                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.<p>
-                        
-                        <h5>Dokumentasi:</h5>
-                        <div class="row">
-                            <div class="col-sm-4">
-                                <img src="https://images.pexels.com/photos/325288/pexels-photo-325288.jpeg" alt="jalan1">
-                            </div>
-                            <div class="col-sm-4">
-                                <img src="https://images.pexels.com/photos/2902747/pexels-photo-2902747.jpeg" alt="jalan2">   
-                            </div>
-                            <div class="col-sm-4">    
-                                <img src="https://images.pexels.com/photos/614484/pexels-photo-614484.jpeg" alt="jalan3">
+                    <div id="report" class="col-sm-12">
+                        <h4 class="card-title">Detail Laporan</h4>
+                        <div class="card">
+                        <div class="card-body">
+                            <table class="table">
+                            <tbody>
+                                <tr>
+                                <td width="35%">Jenis Infrastruktur</td>
+                                <td>:</td>
+                                <td><span id="infra"></span></td>
+                                </tr>
+                                <tr>
+                                <td width="35%">Koordinat Lokasi</td>
+                                <td>:</td>
+                                <td>
+                                    <span id="latitude"></span><br>
+                                    <span id="longitude"></span>
+                                </td>
+                                </tr>
+                                <tr>
+                                <td width="35%">Nama Ruas Jalan</td>
+                                <td>:</td>
+                                <td><span id="ruasjalan"></span></td>
+                                </tr>
+                                <tr>
+                                <td width="35%">Alamat Lengkap</td>
+                                <td>:</td>
+                                <td><span id="lokasi"></span></td>
+                                </tr>
+                            </tbody>
+                            </table>
+                            <h5>Isi Laporan:</h5>
+                            <p id="pengaduan"><p>
+                            
+                            <h5>Dokumentasi:</h5>
+                            <div class="row">
+                                <div class="col-sm-4">
+                                    <img id="dok1" src="" alt="jalan1">
+                                </div>
+                                <div class="col-sm-4">
+                                    <img id="dok2" src="" alt="jalan2">   
+                                </div>
+                                <div class="col-sm-4">    
+                                    <img id="dok3" src="" alt="jalan3">
+                                </div>
                             </div>
                         </div>
-                        
-
-
-                    </div>
                     </div>
                 </div>
             </div>
