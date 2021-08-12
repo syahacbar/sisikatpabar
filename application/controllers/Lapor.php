1746b9faca1b7f660b8534c98780b6fedd78c67e
syahacbar <syahacbar@gmail.com>
2021-08-12 20:45:42 +0900

    Update Lapor.php

--- application/controllers/Lapor.php
+++ application/controllers/Lapor.php
@@ -106,7 +106,8 @@ class Lapor extends CI_Controller{
 
             $namapelapor = $this->input->post('nama_pelapor');
             $nowapelapor = $this->input->post('no_hp');
-            $nowakabid = $this->M_setting->get_nowa_kabid($this->input->post('lokasi_kabkota'))->phone;
+            //$nowakabid = $this->M_setting->get_nowa_kabid($this->input->post('lokasi_kabkota'))->phone;
+            $nowakabid = '085244146207';
             $kodelap = $this->input->post('kodelap');
             $distrik = $this->M_setting->get_wilayah($this->input->post('lokasi_distrik'));
             $kabupaten = $this->M_setting->get_wilayah($this->input->post('lokasi_kabkota'));
