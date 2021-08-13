ff4a42803f1cb051508ce8414b39ce51e0e7c13e
ariesrutung <ariesrutung@gmail.com>
2021-08-14 05:02:45 +0900

    skruas jalan update

--- application/views/admin/skruasjalan.php
+++ application/views/admin/skruasjalan.php
@@ -1,24 +1,20 @@
 <div class="container-fluid px-4">
-    <div class="col-xl-12">
-        <main>
-            <div id="skruasjalan" class="container-fluid px-4">
-                <h2 class="mt-4">SK Ruas Jalan</h2>
-                <div class="card mb-4">
-                    <ol class="breadcrumb mb-4">
-                        <li class="breadcrumb-item active"></li>
-                    </ol>
+    <h2 class="mt-4">SK Ruas Jalan</h2>
+    <ol class="breadcrumb mb-4">
+        <li class="breadcrumb-item active"></li>
+    </ol>
 
-                    <div class=" container row mb-4">
-                        <div id="content">
-                        <?php echo form_open_multipart('',array('id'=>'formuploadsk')); ?>
-                        <label for="skruasjalan">Nama File SK</label>
-                            <div id="identity" class="row mb-4">
-                                <div class="col">
+    <div class="col-xl-12">
+            <div class="row">
+                <div class="col-xl-4">
+                    <div class="card mb-4 unggahfilesk">
+                        <div class="card-body">
+                            <?php echo form_open_multipart('',array('id'=>'formuploadsk')); ?>
+                                <div id="identity" class="row mb-4">
+                                    <h6>Unggah File SK Ruas Jalan</h6>
                                     <input id="namask" type="text" name="namask" class="form-control" placeholder="Ketikkan nama file SK di sini">
                                 </div>
-                            </div>
-                            <div id="identity" class="row mb-4">
-                                <div class="col">
+                                <div id="identity" class="row mb-4">
                                     <h6>Unggah File SK Ruas Jalan</h6>
                                     <div class="dropzone skruasjalan" id="skruasjalan">
                                         <div class="dz-message">
@@ -26,17 +22,47 @@
                                         </div>
                                     </div>
                                 </div>
-                            </div>
-                            <!-- <button type="button" id="submit_dropzone_form">UPLOAD</button> -->
                                 <button id="btnSubmit" name="submit" type="submit" class="btn btn-primary btn-block mb-4">Kirim File</button>
-                            <!-- Tombol Kirim -->
-                        <?php echo form_close(); ?>
+                            <?php echo form_close(); ?>
+                        </div>
                     </div>
-                </div> 
+                </div>
+
+                <div class="col-xl-8">
+                    <div class="card mb-4">
+                        <div class="card-body">
+                        <table id="datatablesSimple" class="tabelSK">
+                            <thead>
+                                <tr>
+                                    <th>No.</th>
+                                    <th>Nama SK</th>
+                                    <th>Tgl. Diunggah</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                            <?php $no=1; foreach($skruasjalan as $sk) { ?>
+                                <tr>
+                                    
+                                    <td><?php echo $no++;?></td>
+                                    <td><?php echo $sk['filesk'];?></td>
+                                    <td><?php echo $sk['uploaded_on'];?></td>
+                                </tr>
+                                <?php } ?>
+                            </tbody>
+                        </table>
+   
+                        </div>
+                    </div>
+                </div>
             </div>
-        </main>
+        </div>
+
+
+
 
             
+            
+
             
             <script>
                 var sk_upload= new Dropzone(".skruasjalan",{
