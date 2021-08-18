1fcddb4661c38df60f235c11fe87c376e4fbb7a5
ariesrutung <ariesrutung@gmail.com>
2021-08-18 10:47:34 +0900

    updade infra adminkab

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -14,7 +14,7 @@ class Admin extends MY_Controller{
         parent::__construct(); 
         $this->load->model('Laporan_model');
         $this->load->model('M_skruasjalan');
-        $this->load->model("M_laporan");
+        $this->load->model("Infrastruktur_model");
         
     }
 
--- application/views/adminkab/infrastruktur.php
+++ application/views/adminkab/infrastruktur.php
@@ -1,9 +1,3 @@
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-<!-- Fontawesome 5.15.3-->
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-<!-- Datatables -->
-<link rel="stylesheet" href="https://cdn.datatables.net/1.10.24/css/dataTables.bootstrap4.min.css">
-
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
@@ -15,6 +9,12 @@
 <script src="https://cdn.datatables.net/1.10.24/js/jquery.dataTables.min.js"></script>
 <script src="https://cdn.datatables.net/1.10.24/js/dataTables.bootstrap4.min.js"></script>
 
+<link rel="stylesheet" href="https://cdn.datatables.net/1.10.24/css/dataTables.bootstrap4.min.css">
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
+<!-- Custom CSS -->
+<link href="<?php echo base_url();?>resources/admintheme/css/tabelinfraadminkab.css" rel="stylesheet" />
 
 <div id="admin" class="container-fluid px-4">
     <div class="row"><br></div>
@@ -49,7 +49,7 @@
                 <div class="panel-body">
 
                 </div>
-                <table class="table table-striped table-bordered table-hover" id="tableInfrastruktur">
+                <table class="table table-striped table-bordered table-hover tabelinfraAdmin" id="tableInfrastruktur">
                     <thead>
                         <tr>
                             <th>No.</th>
